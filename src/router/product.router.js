4537f4cb99bd550d524bb3bcd826b3a192cad32f
hirenpandavips <hirenp.itpath@gmail.com>
2023-05-15 14:54:47 +0530

    Add product API added

---
+++ src/router/product.router.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = new express.Router()
+const multer = require('multer')
+const {
+    addproduct   
+} = require('../controller/product.controller')
+
+const verifytoken = require('../../middleware/verifytoken')
+
+//Creating a storage to store the media files
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {     
+        cb(null, 'public/assets/images');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now()+file.originalname);
+        
+    }
+});
+
+const upload = multer({ storage })
+
+
+////////////Product related routes/////////////
+router.post('/addproduct', upload.single('image'),addproduct)
+
+
+module.exports = router
